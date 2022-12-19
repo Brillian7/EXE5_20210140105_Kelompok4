f13964c5ceac5dbd45f8f50ed2beb0daf62994db
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-19 15:53:46 +0700

    add public program

--- EXE5_20210140105_Kelompok4/Program.cs
+++ EXE5_20210140105_Kelompok4/Program.cs
@@ -11,6 +11,14 @@ namespace EXE5_20210140105_Kelompok4
         int Brillian, Amindia, max = 5;
         int[] queue_array = new int[5];
 
+        public Program()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 indicate
+            * * the queue is initially empty.*/
+            Brillian = -1;
+            Amindia = -1;
+        }
+
         static void Main(string[] args)
         {
         }
