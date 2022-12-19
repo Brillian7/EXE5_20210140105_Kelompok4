6e08d7fd2145dacc01d3eef8e8d5548378938fe0
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-19 15:51:13 +0700

    Add project files.

---
+++ EXE5_20210140105_Kelompok4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140105_Kelompok4
+{
+    class Program
+    {
+        int Brillian, Amindia, max = 5;
+        int[] queue_array = new
+        static void Main(string[] args)
+        {
+        }
+    }
+}
